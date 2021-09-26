a85873e5dff788813b9cedce98391586b7d28a6e
nglis <nglisic10@gmail.com>
2021-09-26 17:22:00 -0600

    notes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// Gets a new object
 var g = G$("John", "Doe");
 
+// Use object on click of login button
 $("#login").bind("click", function () {
+	// Create new Greetr object
 	var loginGrtr = G$("John", "Doe");
 
+	// Hide login on screen
 	$("#logindiv").hide();
 
+	// Fire off HTML greeting, using jQuery selector and chosen language
 	var lang = $("#lang").val();
-	loginGrtr.setLang(lang).HTMLGreeting(greeting, true).log();
+	loginGrtr.setLang(lang).HTMLGreeting("#greeting", true).log();
 });
